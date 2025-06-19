0718377ae62ff4c73dd8087d2d0b29894c3c5861
lipkan02 <lipkan02@gmail.com>
2025-06-19 15:18:28 +0300

    remove flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'todo-client';
-
-  ngOnInit(): void {
-    initFlowbite();
-  }
 }
